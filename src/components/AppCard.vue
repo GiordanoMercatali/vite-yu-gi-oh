2b1d2eb939c3892dc8873e0274739741945e8398
GiordanoMercatali <mercataligiordano.dev@gmail.com>
2023-11-28 10:56:28 +0100

    add: class corrections

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -8,11 +8,12 @@ export default {
 
 <template>
   <div class="mb-4 text-center">
-    <!-- <img :src="card.image" alt="" /> -->
+    <img :src="card.card_images[0].image_url_small" alt="" />
     <h3>{{ card.name }}</h3>
     <p class="text-primary">{{ card.type }}</p>
     <p class="text-secondary">{{ card.race }}</p>
     <p class="text-success">{{ card.attribute }}</p>
+    <p class="text-danger">{{ card.archetype ? card.archetype: "None"}}</p> <!--Archetipo esiste ? Sì (fai questo) : No (fai altro)-->
   </div>
 </template>
 
