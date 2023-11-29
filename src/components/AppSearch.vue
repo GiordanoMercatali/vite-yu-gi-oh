08184bb2202f3bb6c30012589d1bd1dd3fb3c6c8
GiordanoMercatali <mercataligiordano.dev@gmail.com>
2023-11-29 10:15:16 +0100

    changed: dynamic select options (class correction)

--- src/components/AppSearch.vue
+++ src/components/AppSearch.vue
@@ -2,9 +2,10 @@
     <div>
       <select id="archetypes" name="archetype-list" class="form-control" v-model="store.searchText"> 
             <option value="" selected="selected">--Choose an archetype--</option>
-            <option value="alien">Alien</option>
+            <!-- <option value="alien">Alien</option>
             <option value="Ally of Justice">Ally of Justice</option>
-            <option value="Ancient Gear">Ancient Gear</option>
+            <option value="Ancient Gear">Ancient Gear</option> -->
+            <option v-for="(archetype, index) in archetypes" :key="index" :value="archetype">{{ archetype }}</option>
         </select>
       <button @click="$emit('performSearch')" class="btn btn-primary">Search</button>
     </div>
@@ -13,9 +14,15 @@
 <script>
 import {store} from "../store";
 export default {
+  emits: ["performSearch"],
   data() {
     return {
       store,
+      archetypes: [ //Use 'v-for' and an array to declare the 'options' instead of doing it manually
+        "Alien",
+        "Ally of Justice",
+        "Ancient Gear",
+      ]
     };
   },
 };
